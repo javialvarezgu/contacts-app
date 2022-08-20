7965f05393d033c3e9f6bf49b543bcb5ae18ce4b
colinoc <javier.alvarez.guerrero@gmail.com>
2022-08-20 17:35:48 +0200

    Simulate database with JSON file

--- index.php
+++ index.php
@@ -1,91 +1,99 @@
 <?php
 
-$contacts = [
-  ["name"=>"Pepe", "phone_number" => "2313123"],
-  ["name"=>"Manuel", "phone_number" => "23134356"],
-  ["name"=> "Carmen", "phone_number" => "23456343"],
-  ["name"=>"Pepelu", "phone_number" => "299123"],
-  ["name"=>"Manueli", "phone_number" => "23456456356"],
-  ["name"=> "Carmena", "phone_number" => "222222"],
-]
+if (file_exists("contacts.json")) {
+  $contacts = json_decode(file_get_contents("contacts.json"), true);
+} else {
+  $contacts = [];
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.0/darkly/bootstrap.min.css" 
-        integrity="sha512-gUckM9ucxSOwqWuP2kRpTZjtzXfgyKGUlMbXcOq9SAXY+qubqqJTht1XHZvK8rUjFKylKb+gtTK2IiOK3jk4TA==" 
-        crossorigin="anonymous" 
-        referrerpolicy="no-referrer" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script 
-        defer
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" 
-        crossorigin="anonymous">
-    </script>
-    <!-- Stastic Content -->
-    <link rel="stylesheet" href="./static/css/index.css" />
+  <!-- Bootstrap -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css"
+    integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <script
+    defer
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"
+  ></script>
 
-    <title>Contacts App</title>
+  <!-- Static Content -->
+  <link rel="stylesheet" href="./static/css/index.css" />
+
+  <title>Contacts App</title>
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand font-weight-bold" href="#">
+        <img class="mr-2" src="./static/img/logo.png" />
+        ContactsApp
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="add.php">Add Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand font-weight-bold" href="#">
-            <img class="mr-2" src="./static/img/logo.png" />
-            ContactsApp
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="./index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./add.html">Add Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <main>
-        <div class="container pt-4 p-3" >
-            <div class="row"> 
-              <?php foreach ($contacts as $contact): ?>
-                <div class="col-md-4 mb-3">
-                    <div class="card text-center">
-                      <div class="card-body">
-                        <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
-                        <p class="m-2"><?= $contact["phone_number"] ?></p>
-                        <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                        <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-                      </div>
-                    </div>
-                </div>  
-                <?php endforeach ?>
-              
-             
-
+  <main>
+    <div class="container pt-4 p-3">
+      <div class="row">
+        
+        <?php if (count($contacts) == 0): ?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="add.php">Add One!</a>
             </div>
-        </div>
-      </main>
+          </div>
+        <?php endif ?>
+        <?php foreach ($contacts as $contact): ?>
+          <div class="col-md-4 mb-3">
+            <div class="card text-center">
+              <div class="card-body">
+                <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
+                <p class="m-2"><?= $contact["phone_number"] ?></p>
+                <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
+                <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
+              </div>
+            </div>
+          </div>
+        <?php endforeach ?>
 
+      </div>
+    </div>
+  </main>
 </body>
 </html>
