2ccc3a0a9de32b7bdd2d01361e9ae37dc0c1995e
colinoc <javier.alvarez.guerrero@gmail.com>
2022-08-21 20:53:25 +0200

    Validation & bind parameters

--- add.php
+++ add.php
@@ -2,15 +2,26 @@
 
   require "database.php";
 
+  $error = null;
+
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"]) || empty($_POST["phone_number"])){
+      $error= "Please fill all the fields.";
+    } else if  (strlen($_POST["phone_number"]) < 9) {
+      $error= "Phone number must be at least 9 characters.";
+    } else {
       $name = $_POST["name"];
       $phoneNumber = $_POST["phone_number"];
 
-      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
+      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
       $statement->execute();
 
     header("Location: index.php");
+    }
   }
+  
 ?>
 
 <!DOCTYPE html>
@@ -78,6 +89,11 @@
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if ($error != null): ?>
+                <p class="text-danger">
+                  <?= $error ?>
+                </p>
+              <?php endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
