106e3edcbcdf46641515e4005528fc4baf72e7d9
colinoc <javier.alvarez.guerrero@gmail.com>
2022-08-29 18:12:59 +0200

    Create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,8 +4,19 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
+INSERT INTO users (name, email, password) VALUES ("test", "test@test.com", "1234");
+
 CREATE TABLE contacts(
     id INT AUTO_INCREMENT PRIMARY_KEY,
     name VARCHAR(255),
     phone_number VARCHAR(255)
 );
+
+INSERT INTO contacts (name, phone_number) VALUES ("Pepe", "12313424");
